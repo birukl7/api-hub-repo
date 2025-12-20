b05719321dd1ea4d65dd40d6f989d08f49801623
gelead <gelead@users.noreply.github.com>
2025-12-20 19:00:49 +0300

    Add SDK client implementations for lyrics, mealdb, moviedb, news, and newyorktimes APIs

--- api-hub-client/src/index.ts
+++ api-hub-client/src/index.ts
@@ -2,16 +2,31 @@ import { HttpClient } from "./httpClient";
 import { githubApi } from "./github";
 import { cryptoApi } from "./crypto";
 import { youtubeApi } from "./youtube";
+import { lyricsApi } from "./lyrics";
+import { mealdbApi } from "./mealdb";
+import { moviedbApi } from "./moviedb";
+import { newsApi } from "./news";
+import { newyorktimesApi } from "./newyorktimes";
 
 export class ApiHubClient {
   github;
   crypto;
   youtube;
+  lyrics;
+  mealdb;
+  moviedb;
+  news;
+  newyorktimes;
 
   constructor({ baseUrl }: { baseUrl: string }) {
     const client = new HttpClient(baseUrl);
     this.github = githubApi(client);
     this.crypto = cryptoApi(client);
     this.youtube = youtubeApi(client);
+    this.lyrics = lyricsApi(client);
+    this.mealdb = mealdbApi(client);
+    this.moviedb = moviedbApi(client);
+    this.news = newsApi(client);
+    this.newyorktimes = newyorktimesApi(client);
   }
 }
